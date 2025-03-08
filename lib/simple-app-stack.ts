fcdafd3608a301868eba0a9a96c0541e060f1c64
unknown <2867459421@qq.com>
2025-03-09 03:12:26 +0800

    Setup for lab

--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -1,16 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 
 export class SimpleAppStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        // 创建 Lambda 函数
+        const helloLambda = new lambda.Function(this, 'HelloHandler', {
+            runtime: lambda.Runtime.NODEJS_18_X,
+            code: lambda.Code.fromAsset('lambda'),
+            handler: 'hello.handler'
+        });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SimpleAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        // 创建 API Gateway
+        new apigateway.LambdaRestApi(this, 'Endpoint', {
+            handler: helloLambda
+        });
+    }
 }
