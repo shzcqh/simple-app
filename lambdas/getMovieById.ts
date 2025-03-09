3ca795c376818c92762ae1e1b609f4b1962ff78e
unknown <2867459421@qq.com>
2025-03-09 11:05:31 +0800

    Enhanced Lambda debugging and fixed permissions

--- lambdas/getMovieById.ts
+++ lambdas/getMovieById.ts
@@ -5,63 +5,65 @@ import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
 
 const ddbDocClient = createDDbDocClient();
 
-export const handler: Handler = async (event, context) => {
-  try {
-    console.log("Event: ", JSON.stringify(event));
 
-    const queryString = event?.queryStringParameters;
-    const movieId = queryString ? parseInt(queryString.movieId) : undefined;
-
+    export const handler: Handler = async (event, context) => {
+        try {
+          console.log("Event: ", JSON.stringify(event));  
+          const queryString = event?.queryStringParameters;
+          const movieId = queryString ? parseInt(queryString.movieId) : undefined;
+      
+          if (!movieId) {
+            return {
+              statusCode: 404,
+              headers: {
+                "content-type": "application/json",
+              },
+              body: JSON.stringify({ Message: "Missing movie Id" }),
+            };
+          }
+      
+          const commandOutput = await ddbDocClient.send(
+            new GetCommand({
+              TableName: process.env.TABLE_NAME,
+              Key: { id: movieId },
+            })
+          );
+      
+          
+          console.log('GetCommand response:', commandOutput);  
+      
+          if (!commandOutput.Item) {
+            return {
+              statusCode: 404,
+              headers: {
+                "content-type": "application/json",
+              },
+              body: JSON.stringify({ Message: "Invalid movie Id" }),
+            };
+          }
+      
+          const body = { data: commandOutput.Item };
+      
+          return {
+            statusCode: 200,
+            headers: {
+              "content-type": "application/json",
+            },
+            body: JSON.stringify(body),
+          };
+        } catch (error: any) {
+          console.log(JSON.stringify(error));  
+          return {
+            statusCode: 500,
+            headers: {
+              "content-type": "application/json",
+            },
+            body: JSON.stringify({ error }),
+          };
+        }
+      };
+      
    
-    if (!movieId) {
-      return {
-        statusCode: 404,
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify({ Message: "Missing movie Id" }),
-      };
-    }
-
-    
-    const commandOutput = await ddbDocClient.send(
-      new GetCommand({
-        TableName: process.env.TABLE_NAME,
-        Key: { id: movieId },
-      })
-    );
-
-    
-    if (!commandOutput.Item) {
-      return {
-        statusCode: 404,
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify({ Message: "Invalid movie Id" }),
-      };
-    }
-
-    const body = { data: commandOutput.Item };
-
-    return {
-      statusCode: 200,
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(body),
-    };
-  } catch (error: any) {
-    console.log(JSON.stringify(error));
-    return {
-      statusCode: 500,
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({ error }),
-    };
-  }
-};
 
 function createDDbDocClient() {
   const ddbClient = new DynamoDBClient({ region: process.env.REGION });
